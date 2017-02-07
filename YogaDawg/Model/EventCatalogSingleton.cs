c79c845fdc4d722b7183d348e986f8b1cc343ff5
anne55x9 <anne55x9@edu.easj.dk>
2017-02-07 17:49:30 +0100

    Lavet en eventCatalogSingleton klasse med en eentlist af observablecollection typen. I konstruktøren er 2 events initialiseret dog uden datetime for nu.

---
+++ YogaDawg/Model/EventCatalogSingleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YogaDawg.Model
+{
+    class EventCatalogSingleton 
+    {
+        public ObservableCollection<Event> EventList { get; set; }
+
+        public EventCatalogSingleton()
+        {
+            this.EventList.Add(new Event(20,"Yogi1", "Flow", "KBH"));
+            this.EventList.Add(new Event(10, "Yogi2", "Hatcha", "KBH"));
+        }
+
+    }
+}
