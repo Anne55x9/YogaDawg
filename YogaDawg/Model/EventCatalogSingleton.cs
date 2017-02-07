04e5d0fb8e650fc2b1742dc49a0007f3ede8fa8e
anne55x9 <anne55x9@edu.easj.dk>
2017-02-07 17:56:36 +0100

    Tilføjet en AddEvent metode i EventCatalogSingleton samt implementeret INotifyPropertyChanged interfasen i samme klasse.

--- YogaDawg/Model/EventCatalogSingleton.cs
+++ YogaDawg/Model/EventCatalogSingleton.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace YogaDawg.Model
 {
-    class EventCatalogSingleton 
+    class EventCatalogSingleton: INotifyPropertyChanged   
     {
         public ObservableCollection<Event> EventList { get; set; }
 
@@ -15,6 +16,19 @@ namespace YogaDawg.Model
         {
             this.EventList.Add(new Event(20,"Yogi1", "Flow", "KBH"));
             this.EventList.Add(new Event(10, "Yogi2", "Hatcha", "KBH"));
+        }
+
+        
+
+        public void AddEvent(Event NyEvent)
+        {
+            EventList.Add(NyEvent);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
         }
 
     }
