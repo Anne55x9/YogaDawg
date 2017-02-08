8dd1673f9eb27785237324389dd49183ca433066
anne55x9 <anne55x9@edu.easj.dk>
2017-02-08 10:34:42 +0100

    Lavet full prop i eventViewModel og inkluderet Date Time initialisering i EventViewModel konstruktøren.

--- YogaDawg/ViewModel/EventViewModel.cs
+++ YogaDawg/ViewModel/EventViewModel.cs
@@ -12,5 +12,60 @@ namespace YogaDawg.ViewModel
     {
         public EventCatalogSingleton EventList { get; set; }
 
+        private int _id;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _description;
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        private string _place;
+
+        public string Place
+        {
+            get { return _place; }
+            set { _place = value; }
+        }
+
+        private DateTimeOffset _date;
+
+        public DateTimeOffset Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        private TimeSpan _time;
+
+        public TimeSpan Time
+        {
+            get { return _time; }
+            set { _time = value; }
+        }
+
+        public EventViewModel()
+        {
+            DateTime dt = System.DateTime.Now;
+            _date = new DateTimeOffset(dt.Year,dt.Month,dt.Day,0,0,0,0, new TimeSpan());
+            _time = new TimeSpan(dt.Hour,dt.Minute, dt.Second);
+        }
+
     }
 }
